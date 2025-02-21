00bf9e4b9ac97cfede4c35ff91edb02bc10ece9f
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-02-21 23:04:13 +0300

    Added easy-requester package

---
+++ packages/easy-requester/src/index.ts
@@ -0,0 +1,3 @@
+import BaseRequester from "./core/baseRequester";
+import type { Methods, HttpProtocols, ClientConfig, RequestConfig } from "./types/globals";
+export { BaseRequester as EasyRequester, Methods, HttpProtocols, ClientConfig, RequestConfig };
