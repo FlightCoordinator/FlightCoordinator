b76ebae7596e3bd737a6de8b5fab584cf7b5c9d1
Çağan Seyrek <caganseyrek@outlook.com.tr>
2025-02-24 15:11:29 +0300

    Deleted legacy/older-web-client

--- legacy/older-web-client/src/types/globals.d.ts
+++
@@ -1,8 +0,0 @@
-namespace GlobalTypes {
-  export interface ServerResponseParams<TResponseData = null> {
-    isSuccess: boolean;
-    message: string;
-    data?: TResponseData;
-  }
-}
-export default GlobalTypes;
